c83269bb562fba31808eb27327decc598af27cd3
Mauricio Arroyave Alzate <mauricio.arroyave@nordstrom.com>
2022-11-27 10:54:09 -0500

    Adding tab bar navigation and screens

--- App.tsx
+++ App.tsx
@@ -1,19 +1,62 @@
 import React from 'react';
-import {ThemeProvider, Text} from 'react-native-elements';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {ThemeProvider} from 'react-native-elements';
+import SvgUri from 'react-native-svg-uri';
+
+// @navigation vendors
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+
+// @screens
+import FavoritesScreen from './src/screens/FavoritesScreen/FavoritesScreen';
+import HomeScreen from './src/screens/HomeScreen/HomeScreen';
 
 // @theme
 import {theme} from './src/mainTheme';
 
+// @assets
+const homeIcon = require('./src/assets/svg/home');
+const favoritesIcon = require('./src/assets/svg/favorite');
+
 const App = () => {
+  const Tab = createBottomTabNavigator();
+
+  const MyTabs = () => (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({route}) => ({
+        tabBarIcon: () => {
+          let icon = 'home';
+          if (route.name === 'Home') {
+            icon = homeIcon;
+          }
+          if (route.name === 'Favorites') {
+            icon = favoritesIcon;
+          }
+          return <SvgUri fill="red" height={20} width={20} svgXmlData={icon} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'black',
+        inactiveTintColor: 'gray',
+      }}>
+      <Tab.Screen
+        options={{headerShown: false}}
+        name="Home"
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        options={{headerShown: false}}
+        name="Favorites"
+        component={FavoritesScreen}
+      />
+    </Tab.Navigator>
+  );
+
   return (
     <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <StatusBar barStyle={'light-content'} backgroundColor="white" />
-        <Text>hola</Text>
-        <Text>hola</Text>
-        <Text>hola</Text>
-      </SafeAreaView>
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
     </ThemeProvider>
   );
 };
---
+++ src/screens/FavoritesScreen/FavoritesScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Text} from 'react-native-elements';
+
+const FavoritesScreen = () => {
+  return (
+    <View>
+      <Text>Favorites Screen</Text>
+    </View>
+  );
+};
+
+export default FavoritesScreen;
